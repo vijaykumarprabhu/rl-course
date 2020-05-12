20be9edd79c204ea576a22c71378f801153581fa
vijaykumarprabhu <vijay.pesit@gmail.com>
2020-05-12 10:51:59 +0200

    value iteration algorithm

--- ex03-dynp/ex03-dynp.py
+++ ex03-dynp/ex03-dynp.py
@@ -3,6 +3,13 @@ import numpy as np
 
 # Init environment
 env = gym.make("FrozenLake-v0")
+# custom_map3x3 = [
+#     'SFF',
+#     'FFF',
+#     'FHG',
+# ]
+# env = gym.make("FrozenLake-v0", desc=custom_map3x3)
+
 # you can set it to deterministic with:
 # env = gym.make("FrozenLake-v0", is_slippery=False)
 
@@ -22,7 +29,31 @@ def value_iteration():
     gamma = 0.8
     # TODO: implement the value iteration algorithm and return the policy
     # Hint: env.P[state][action] gives you tuples (p, n_state, r, is_terminal), which tell you the probability p that you end up in the next state n_state and receive reward r
+    iterations = 0
+    policy = np.zeros(n_states, dtype=np.int) 
+    while True:
+    	delta = 0.0
+    	iterations += 1
+    	for state in range(n_states):
+    		v = V_states[state]
+    		max_action_val = -9999
+    		for action in range(n_actions):
+    			summation = 0.0
+    			for p, n_state, r, is_terminal  in env.P[state][action]:
+    				summation += p * (r + gamma* V_states[n_state])
+    			if summation > max_action_val:
+    				max_action_val = summation
+    				policy[state] = action
+    		V_states[state] = max_action_val
+    		
+    		delta = max(delta, abs(v-V_states[state]))
+    	if delta < theta:
+    		break
+    print("steps to converge", iterations)
+    print("optimal value function",V_states)
 
+    ## computing optimal policy
+    return policy
 
 def main():
     # print the environment
